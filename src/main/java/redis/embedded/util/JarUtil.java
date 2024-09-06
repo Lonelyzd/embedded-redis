a3874c4883cbb9bfa413a5d1df63334fa2ccc4ce
Lonely <1156607853@qq.com>
2024-09-06 10:52:04 +0800

    修复Redis调用示例中，创建临时目录失败BUG

--- src/main/java/redis/embedded/util/JarUtil.java
+++ src/main/java/redis/embedded/util/JarUtil.java
@@ -1,16 +1,20 @@
 package redis.embedded.util;
 
-import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermissions;
 
 public class JarUtil {
 
     public static File extractExecutableFromJar(String executable) throws IOException {
-        File tmpDir = Files.createTempDir();
+//        File tmpDir = Files.createTempDir();
+        File tmpDir = Files.createTempDirectory("tempDir").toFile();
         tmpDir.deleteOnExit();
 
         File command = new File(tmpDir, executable);
